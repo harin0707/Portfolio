523d73234ac56e0c949fd787337e3d1b68032473
Yu Harin <101720703+harin0707@users.noreply.github.com>
2022-04-30 04:34:52 +0900

    Add files via upload

---
+++ day-night.js
@@ -0,0 +1,18 @@
+const checkbox = document.querySelector('.checkbox');
+console.log(checkbox)
+checkbox.addEventListener('click', toggleClick);
+
+// function click(){
+//     if (document.body.classList.contains('dark')){
+//         document.body.classList.remove('dark');
+//         // console.log("convert into Light Mode");
+//     }else{
+//         document.body.classList.add('dark');
+//         // console.log("convert into Dark Mode");
+//     }
+// }
+
+function toggleClick() {
+    document.body.classList.toggle('dark');
+    console.log('Working!');
+}
